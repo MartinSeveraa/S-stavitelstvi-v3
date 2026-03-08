6ebbc875ef3e162c420534dfca42c59eb7da8bdc
MartinSevera <st75788@upce.cz>
2026-03-08 23:30:56 +0100

    ahrefs data

---
+++ src/app/routes.tsx
@@ -0,0 +1,11 @@
+// routes.ts
+import { createHashRouter } from 'react-router-dom'  
+import Home from './pages/Home'
+
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  // ... ostatní routes
+])
